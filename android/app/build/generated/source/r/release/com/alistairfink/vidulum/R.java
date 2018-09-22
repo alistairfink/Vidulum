0c1190c3dfa996eb558c1851ee5561c988ef834a
Unknown <alistairfink@gmail.com>
2018-09-21 22:05:26 -0400

    New Build - 21-09-2018

--- android/app/build/generated/source/r/release/com/alistairfink/vidulum/R.java
+++ android/app/build/generated/source/r/release/com/alistairfink/vidulum/R.java
@@ -1823,13 +1823,14 @@ containing a value of this type.
         public static final int assets_menuicon=0x7f020048;
         public static final int assets_numpaddeleteicon=0x7f020049;
         public static final int assets_okicon=0x7f02004a;
-        public static final int assets_refreshicon=0x7f02004b;
-        public static final int assets_sendicon=0x7f02004c;
-        public static final int assets_settingsicon=0x7f02004d;
-        public static final int assets_transactionicon=0x7f02004e;
-        public static final int node_modules_reactnavigation_src_views_assets_backicon=0x7f02004f;
-        public static final int node_modules_reactnavigation_src_views_assets_backiconmask=0x7f020050;
-        public static final int notification_template_icon_bg=0x7f020051;
+        public static final int assets_profile=0x7f02004b;
+        public static final int assets_refreshicon=0x7f02004c;
+        public static final int assets_sendicon=0x7f02004d;
+        public static final int assets_settingsicon=0x7f02004e;
+        public static final int assets_transactionicon=0x7f02004f;
+        public static final int node_modules_reactnavigation_src_views_assets_backicon=0x7f020050;
+        public static final int node_modules_reactnavigation_src_views_assets_backiconmask=0x7f020051;
+        public static final int notification_template_icon_bg=0x7f020052;
     }
     public static final class id {
         public static final int action0=0x7f0d0058;
